72c3b50e0b0e672ef3d03cdfdbbdce9bdc02bebb
Geniecko <radoslawkania09@gmail.com>
2022-07-22 16:24:06 +0200

    Add react icons to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaBeer } from 'react-icons/fa';
 
 const Headline = styled.h1`
   color: ${({ color }) => color || 'green'};
@@ -10,6 +11,7 @@ function App() {
     <>
       <Headline>Hello</Headline>
       <Headline>Hello</Headline>
+      <FaBeer />
     </>
   );
 }
