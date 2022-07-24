fba61b47339bb0f312bae954bc4f39252a0b99f7
Geniecko <radoslawkania09@gmail.com>
2022-07-24 19:55:16 +0200

    install react-router-dom, fix styled components(dev tools), add variable

--- src/theme/GlobalStyle.jsx
+++ src/theme/GlobalStyle.jsx
@@ -12,10 +12,39 @@ const GlobalStyle = createGlobalStyle`
   body{
     margin: 0;
     padding: 0;
+    min-height: 100vh;
     font-size: 1rem;
     font-family: 'Barlow', sans-serif;
     background-color: ${({ theme }) => theme.color.pageBackground};
-  } 
+    color: ${({ theme }) => theme.color.secondary};
+  }
+
+  h1,h2,h3,h4,h5{
+    font-weight: 700;
+    margin: 0;
+    margin-bottom: 1.6rem;
+  }
+
+  h1{
+    font-size: 2.8rem;
+  }
+
+  h2{
+    font-size: 2.2rem;
+  }
+
+  h3{
+    font-size: 1.8rem;
+  }
+
+  p{
+    font-size: 1.6rem;
+    margin-bottom: 1.2rem;
+
+    &:last-child{
+      margin-bottom: 0;
+    }
+  }
 `;
 
 export default GlobalStyle;
